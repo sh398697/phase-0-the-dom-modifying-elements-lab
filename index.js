a43c76a81b2a8c9f0a69037bd21c1f4e26828c5a
Scott <scotthenry1@gmail.com>
2023-01-23 22:48:43 -0500

    Completed lab submission

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+const element = document.getElementById("main");
+element.remove();
+
+
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Scott is the champion"
+document.body.append(newHeader);
